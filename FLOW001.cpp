07e465fb5d63fa1c0478b99ced476027725f6dea
G.Vageesha Datta <43772803+VageeshaDatta@users.noreply.github.com>
2022-04-04 09:34:39 +0530

    Update FLOW001.cpp

--- FLOW001.cpp
+++ FLOW001.cpp
@@ -1,6 +1,3 @@
-// You don't need to add/edit anything to the below solution. 
-// Click on the SUBMIT button to solve your first problem on CodeChef.
-
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
