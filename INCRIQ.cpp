44123098f6ccffea2d4270d347f670a028f9edb8
G.Vageesha Datta <43772803+VageeshaDatta@users.noreply.github.com>
2022-04-04 09:33:51 +0530

    Add files via upload

---
+++ INCRIQ.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int X;
+    cin >> X;
+    if(X>163)
+           cout << "yes"   << endl;
+    else
+           cout << "no" << endl;
+        
+   
+	// your code goes here
+	return 0;
+}
---
+++ THREETOPICS.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t1,t2,t3,X;
+    cin >> t1 >> t2 >> t3 >> X ;
+    if(X==t1 || X==t2 || X==t3)
+        cout  << "Yes" << endl;
+    else    
+       cout << "No" << endl;
+	// your code goes here
+	return 0;
+}
